24ca555c0b0d2e4b3266a422a243df371791a09b
mdagnisse-cyber <mdagnisse@gmail.com>
2026-05-03 00:52:50 +0100

    Remove database connection setup from config.php
    Removed database connection code from config.php.

--- config.php
+++ config.php
@@ -3,14 +3,4 @@ $host = "sql202.infinityfree.com";
 $user = "if0_39714282"; 
 $pass = "MonDomaine1234"; 
 $dbname = "if0_39714282_epreuve_db"; 
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-// Vérification de la connexion
-if ($conn->connect_error) {
-    die("Erreur de connexion : " . $conn->connect_error);
-}
-
-// Optionnel : définir l'encodage
-$conn->set_charset("utf8");
 ?>
